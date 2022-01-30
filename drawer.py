d200934d06251064fa4c9fe3036a28596ec95a5b
wacfeld <wacfeldwang333@gmail.com>
2022-01-29 20:18:59 -0500

    blank board w/ highlight

--- drawer.py
+++ drawer.py
@@ -1,21 +1,68 @@
 from graphics import *
-import random, time
+import math
 
-win = GraphWin("Random Circles", 300, 300)
-for i in range(75):
-    r = random.randrange(256)
-    b = random.randrange(256)
-    g = random.randrange(256)
-    color = color_rgb(r, g, b)
-    
-    radius = random.randrange(3, 40)
-    x = random.randrange(5, 295)
-    y = random.randrange(5, 295)
-    
-    circle = Circle(Point(x,y), radius)
-    circle.setFill(color)
-    circle.draw(win)
-    time.sleep(.05)
-    
-win.promptClose(win.getWidth()/2, 20)
+'''
+make a chessboard
+figure out where clicks land
+put pieces on the chessboard
+move pieces on the chessboard
+'''
 
+def makeRect(corner, width, height):
+    '''Return a new Rectangle given one corner Point and the dimensions.'''
+    corner2 = corner.clone()
+
+    corner2.move(width, height)
+    return Rectangle(corner, corner2)
+
+def main():
+    squareside = 50 # side length of board square
+
+    # square colors
+    colors = [color_rgb(240,114,114), color_rgb(241,241,201)] # black, white, highlight
+    hlcol = color_rgb(42,255,42)
+    curcol = 0
+
+    # dimensions of board
+    width = 8
+    height = 8
+
+    win = GraphWin("board", squareside*width, squareside*height) # init window
+    win.yUp() # y axis goes bottom to top
+
+    squares = [[None for y in range(height)] for x in range(width)] # 2d array
+
+    for x in range(8):
+        for y in range(8):
+            curx = x*squareside
+            cury = y*squareside
+            r = makeRect(Point(curx, cury), squareside, squareside)
+            squares[x][y] = r
+            r.setFill(colors[curcol])
+            r.setWidth(0)
+            r.draw(win)
+
+            # toggle color
+            curcol = (-1 * curcol) + 1
+        curcol = (-1 * curcol) + 1
+
+    print(squares)
+    
+    curhl = None
+    while True:
+        p = win.getMouse()
+        if curhl != None:
+            curhl.undraw()
+        print(p)
+        px = p.getX()
+        py = p.getY()
+        rank = math.floor(px/squareside)
+        file = math.floor(py/squareside)
+        r = squares[rank][file]
+        curhl = r.clone()
+        curhl.setFill(hlcol)
+        curhl.draw(win)
+        
+
+if __name__ == '__main__':
+    main()
